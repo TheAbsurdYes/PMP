64856ad14fb77eb09caaf634f382811c1be7af2b
TheAbsurdYes <adrianapostol89@gmail.com>
2020-10-21 23:03:09 +0300

    Update Ex1.scala

--- src/main/scala/Lab2/Ex1.scala
+++ src/main/scala/Lab2/Ex1.scala
@@ -1,21 +1,18 @@
 package Lab2
 
-import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
-
 abstract class Persoana
 
 class Student(var nume: String, var prenume: String, var an: Int, var materii: Array[(String,Int)]) extends Persoana{
     def setNota(materie: String, nota: Int){
-        materii.indexOf((materie,_))._2 = nota 
+        materii(materii.indexWhere(_._1 == materie)) = materii(materii.indexWhere(_._1 == materie)).copy(_2 = nota) 
     }
     
     def addMaterie(materie: String, nota: Int){
-        materii = materii +: (materie,nota)
+        materii :+= (materie,nota)
     }
 
     def getNota(materie: String){
-        println(materii.indexOf((materie,_))._2)
+        materii.foreach(x => if (x._1 == materie) println(x._2))
     }
 }
 
@@ -36,8 +33,10 @@ object Ex1{
         val a2 = new Student("Marinela","Alelia",1,list zip Array.fill(3){scala.util.Random.nextInt(11)})
         val a3 = new Student("Gica","Petrescu",1,list zip Array.fill(3){scala.util.Random.nextInt(11)})
         whatIs(a1)
+        a2.materii.foreach(println)
         a2.addMaterie("Ag",3)
+        a2.materii.foreach(println)
         a2.setNota("Ag",10)
         a2.getNota("Ag")
-    }
+    }    
 }
