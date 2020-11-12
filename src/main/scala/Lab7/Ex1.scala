2abf412a243ddc7297a12d804d4a9def063151e6
TheAbsurdYes <adrianapostol89@gmail.com>
2020-11-12 10:47:00 +0200

    Lab7

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -1,16 +1,17 @@
 package Lab7
 
-import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.experimental.normalproposals.Beta
+import com.cra.figaro.library.atomic.continuous.Uniform
 
 object Ex1 {
-	def main(args: Array[String]) : Unit = {
-		val test = Constant("Test")
+	def greaterThan4(d: Double): Boolean = d > 3.9
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		
-		println(algorithm.probability(test, "Test"))
+	def main(args: Array[String]) : Unit = {
+		val par = Beta(3,5)
+		println(s"par = ${par}")
+		println("Bias")
+		// Nu stiu mi se pare prea stupid, nu inteleg nici beta ce inseamna, nici uniform, nici binomial , si nici contextul problemei nu am reusit sa il inteleg.
+		// Nu am probleme cu scala dar stilul asta de programare ma depaseste.
 	}
 }
